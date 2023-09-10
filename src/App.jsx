4d3e9535ef772057c3c5446635ddc5a40179f08a
Muhammad Fahad <fahadkh625@gmail.com>
2023-09-11 00:53:02 +0500

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Muhammad Fahad",
+  title: "Data Engineer",
+  email: "muhammad.fahad@studentambassadors.com",
+  gitHub: "fahada8",
+  instagram: "",
+  linkedIn: "fahada08",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "fahada08",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
